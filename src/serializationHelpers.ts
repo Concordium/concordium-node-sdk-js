be5e96a0f2833542c6ab189da8a6620b03976a05
omkarsunku <omkar@blocktech.dk>
2022-01-18 17:03:29 +0530

    lint fix issues

--- src/serializationHelpers.ts
+++ src/serializationHelpers.ts
@@ -75,7 +75,7 @@ export function encodeWord128(value: bigint): Buffer {
     if (value > 170141183460469231731687303715884105728n || value < 0n) {
         throw new Error(
             'The input has to be a 128 bit unsigned integer but it was: ' +
-            value
+                value
         );
     }
     const arr = new ArrayBuffer(32);
@@ -607,8 +607,8 @@ export function serializeMapType(
 export function serializeEnumType(
     enumType: EnumType,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
-    enumData: any)
-    : Buffer {
+    enumData: any
+): Buffer {
     const bufferArray: Buffer[] = [];
     bufferArray.push(encodeWord8(enumData as number));
     return Buffer.concat(bufferArray);

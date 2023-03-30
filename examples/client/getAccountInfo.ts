d1d61404687f4f9aca0235e9f36147d3f659acfb
rasmus-kirk <mail@rasmuskirk.com>
2023-03-30 12:21:07 +0200

    lint works

--- examples/client/getAccountInfo.ts
+++ examples/client/getAccountInfo.ts
@@ -4,36 +4,40 @@ import { credentials } from '@grpc/grpc-js';
 
 import meow from 'meow';
 
-const cli = meow(`
-	Usage
-	  $ yarn example getAccountInfo
+const cli = meow(
+    `
+  Usage
+    $ yarn example getAccountInfo
 
-	Options
-  --endpoint,  -e  Specify endpoint of the form "address:port"
-  --account,   -a  An account address to get info from
-  --blockhash, -b  A blockhash to query the info from
-`, {
-  importMeta: import.meta,
-	flags: {
-		endpoint: {
-			type: 'string',
-			shortFlag: 'e',
-      default: 'node.testnet.concordium.com:20000'
-		},
-    account: {
-      type: 'string',
-      short: 'a',
-      default: '3kBx2h5Y2veb4hZgAJWPrr8RyQESKm5TjzF3ti1QQ4VSYLwK1G'
-    },
-    blockhash: {
-      type: 'string',
-      short: 'b',
-      default: 'fe88ff35454079c3df11d8ae13d5777babd61f28be58494efe51b6593e30716e'
+  Options
+    --endpoint,  -e  Specify endpoint of the form "address:port"
+    --account,   -a  An account address to get info from
+    --blockhash, -b  A blockhash to query the info from
+`,
+    {
+        importMeta: import.meta,
+        flags: {
+            endpoint: {
+                type: 'string',
+                shortFlag: 'e',
+                default: 'node.testnet.concordium.com:20000',
+            },
+            account: {
+                type: 'string',
+                short: 'a',
+                default: '3kBx2h5Y2veb4hZgAJWPrr8RyQESKm5TjzF3ti1QQ4VSYLwK1G',
+            },
+            blockhash: {
+                type: 'string',
+                short: 'b',
+                default:
+                    'fe88ff35454079c3df11d8ae13d5777babd61f28be58494efe51b6593e30716e',
+            },
+        },
     }
-	}
-});
+);
 
-const [address, port] = cli.flags.endpoint.split(':')
+const [address, port] = cli.flags.endpoint.split(':');
 const client = createConcordiumClient(
     address,
     Number(port),

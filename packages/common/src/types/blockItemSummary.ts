421fe5c4d498e941a60fb584da55ed988a0b2d86
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-05-01 12:38:27 +0200

    Helpers for extracting data from transactions

--- packages/common/src/types/blockItemSummary.ts
+++ packages/common/src/types/blockItemSummary.ts
@@ -18,6 +18,7 @@ import {
     MemoEvent,
     ModuleDeployedEvent,
     NewEncryptedAmountEvent,
+    TransactionEventTag,
     TransferredWithScheduleEvent,
 } from './transactionEvent';
 import { UpdateInstructionPayload } from './chainUpdate';
@@ -25,8 +26,11 @@ import {
     HexString,
     TransactionSummaryType,
     TransactionStatusEnum,
+    ContractAddress,
+    Base58String,
 } from '../types';
 import { RejectReason } from './rejectReason';
+import { isDefined } from '../serializationHelpers';
 
 export interface BaseBlockItemSummary {
     index: bigint;
@@ -252,3 +256,201 @@ export type BlockItemStatus =
     | CommittedBlockItem
     | FinalizedBlockItem
     | PendingBlockItem;
+
+export const isInitContractSummary = (
+    summary: BlockItemSummary
+): summary is BaseAccountTransactionSummary & InitContractSummary =>
+    summary.type === TransactionSummaryType.AccountTransaction &&
+    summary.transactionType === TransactionKindString.InitContract;
+
+export const isUpdateContractSummary = (
+    summary: BlockItemSummary
+): summary is BaseAccountTransactionSummary & UpdateContractSummary =>
+    summary.type === TransactionSummaryType.AccountTransaction &&
+    summary.transactionType === TransactionKindString.Update;
+
+export const isTransferLikeSummary = (
+    summary: BlockItemSummary
+): summary is BaseAccountTransactionSummary &
+    (TransferSummary | TransferWithMemoSummary) =>
+    summary.type === TransactionSummaryType.AccountTransaction &&
+    (summary.transactionType === TransactionKindString.Transfer ||
+        summary.transactionType === TransactionKindString.TransferWithMemo);
+
+export const isTransferWithScheduleLikeSummary = (
+    summary: BlockItemSummary
+): summary is BaseAccountTransactionSummary &
+    (TransferWithScheduleSummary | TransferWithScheduleAndMemoSummary) =>
+    summary.type === TransactionSummaryType.AccountTransaction &&
+    (summary.transactionType === TransactionKindString.TransferWithSchedule ||
+        summary.transactionType ===
+            TransactionKindString.TransferWithScheduleAndMemo);
+
+export const isSuccessTransaction = (summary: BlockItemSummary): boolean => {
+    if (summary.type !== TransactionSummaryType.AccountTransaction) {
+        return true;
+    }
+
+    return summary.transactionType !== TransactionKindString.Failed;
+};
+
+export function getTransactionRejectReason(
+    summary: AccountCreationSummary | UpdateSummary
+): undefined;
+export function getTransactionRejectReason(
+    summary: BlockItemSummary
+): RejectReason | undefined;
+export function getTransactionRejectReason(
+    summary: BlockItemSummary
+): RejectReason | undefined {
+    if (!isRejectTransaction(summary)) {
+        return undefined;
+    }
+
+    return summary.rejectReason;
+}
+
+export const isRejectTransaction = (
+    summary: BlockItemSummary
+): summary is BaseAccountTransactionSummary & FailedTransactionSummary =>
+    summary.type === TransactionSummaryType.AccountTransaction &&
+    summary.transactionType === TransactionKindString.Failed;
+
+const isEqualContract =
+    (a: ContractAddress) =>
+    (b: ContractAddress): boolean =>
+        a.index === b.index && a.subindex === b.subindex;
+
+export function affectedContracts(
+    summary: AccountCreationSummary | UpdateSummary
+): never[];
+export function affectedContracts(summary: BlockItemSummary): ContractAddress[];
+export function affectedContracts(
+    summary: BlockItemSummary
+): ContractAddress[] {
+    if (summary.type !== TransactionSummaryType.AccountTransaction) {
+        return [];
+    }
+
+    switch (summary.transactionType) {
+        case TransactionKindString.InitContract: {
+            return [summary.contractInitialized.address];
+        }
+        case TransactionKindString.Update: {
+            return summary.events.reduce(
+                (addresses: ContractAddress[], event) => {
+                    if (
+                        event.tag !== TransactionEventTag.Updated ||
+                        addresses.some(isEqualContract(event.address))
+                    ) {
+                        return addresses;
+                    }
+
+                    return [...addresses, event.address];
+                },
+                []
+            );
+        }
+        default: {
+            return [];
+        }
+    }
+}
+
+export function getReceiverAccount(
+    summary: AccountCreationSummary | UpdateSummary
+): undefined;
+export function getReceiverAccount(
+    summary: BlockItemSummary
+): Base58String | undefined;
+export function getReceiverAccount(
+    summary: BlockItemSummary
+): Base58String | undefined {
+    if (summary.type !== TransactionSummaryType.AccountTransaction) {
+        return undefined;
+    }
+
+    switch (summary.transactionType) {
+        case TransactionKindString.Transfer:
+        case TransactionKindString.TransferWithMemo:
+        case TransactionKindString.TransferWithScheduleAndMemo:
+            return summary.transfer.to;
+        case TransactionKindString.TransferWithSchedule:
+            return summary.event.to;
+    }
+}
+
+export function affectedAccounts(
+    summary: AccountCreationSummary | UpdateSummary
+): never[];
+export function affectedAccounts(summary: BlockItemSummary): Base58String[];
+export function affectedAccounts(summary: BlockItemSummary): Base58String[] {
+    if (summary.type !== TransactionSummaryType.AccountTransaction) {
+        return [];
+    }
+
+    switch (summary.transactionType) {
+        case TransactionKindString.EncryptedAmountTransfer:
+        case TransactionKindString.EncryptedAmountTransferWithMemo:
+            return [summary.added.account, summary.removed.account];
+        case TransactionKindString.TransferToEncrypted:
+            return [summary.added.account];
+        case TransactionKindString.TransferToPublic:
+            return [summary.removed.account];
+        case TransactionKindString.Update: {
+            return summary.events.reduce((addresses: Base58String[], event) => {
+                if (
+                    event.tag === TransactionEventTag.Transferred &&
+                    event.to.type === 'AddressAccount' &&
+                    !addresses.includes(event.to.address)
+                ) {
+                    return [...addresses, event.to.address];
+                }
+                return addresses;
+            }, []);
+        }
+        default: {
+            const receiver = getReceiverAccount(summary);
+
+            if (summary.sender === receiver || receiver === undefined) {
+                return [summary.sender];
+            }
+
+            return [summary.sender, receiver];
+        }
+    }
+}
+
+export type SummaryContractUpdateLog = {
+    address: ContractAddress;
+    events: HexString[];
+};
+
+export function getSummaryContractUpdateLogs(
+    summary: AccountCreationSummary | UpdateSummary
+): never[];
+export function getSummaryContractUpdateLogs(
+    summary: BlockItemSummary
+): SummaryContractUpdateLog[];
+export function getSummaryContractUpdateLogs(
+    summary: BlockItemSummary
+): SummaryContractUpdateLog[] {
+    if (
+        summary.type !== TransactionSummaryType.AccountTransaction ||
+        !isUpdateContractSummary(summary)
+    ) {
+        return [];
+    }
+
+    return summary.events
+        .map((event) => {
+            switch (event.tag) {
+                case TransactionEventTag.Updated:
+                case TransactionEventTag.Interrupted:
+                    return { address: event.address, events: event.events };
+                default:
+                    return undefined;
+            }
+        })
+        .filter(isDefined);
+}

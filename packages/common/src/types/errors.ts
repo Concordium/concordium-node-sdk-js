d6f777b537b55f5c5a0c3fb67cea073a07e2d5aa
rasmus-kirk <mail@rasmuskirk.com>
2023-05-04 15:22:10 +0200

    Did more rogerous checking of isRpcError function

--- packages/common/src/types/errors.ts
+++ packages/common/src/types/errors.ts
@@ -3,5 +3,11 @@ import { RpcError } from '@protobuf-ts/runtime-rpc';
 export { RpcError };
 
 export function isRpcError(error: unknown): error is RpcError {
-    return (error as RpcError).code !== undefined;
+    const err = error as RpcError;
+    return (
+        err.code !== undefined &&
+        err.meta !== undefined &&
+        err.name !== undefined &&
+        err.toString !== undefined
+    );
 }

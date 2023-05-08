bf35d29e9b13e744551cb0f9973e61519af4f6d1
rasmus-kirk <mail@rasmuskirk.com>
2023-05-08 13:46:54 +0200

    Added more examples:
     - getTokenomicsInfo
     - instanceStateLookup
     - invokeContract
     - peerConnect
     - peerDisconnect
     - shutdown
     - unbanPeer

---
+++ examples/client/shutdown.ts
@@ -0,0 +1,42 @@
+import { createConcordiumClient } from '@concordium/node-sdk';
+import { credentials } from '@grpc/grpc-js';
+
+import meow from 'meow';
+
+const cli = meow(
+    `
+  Usage
+    $ yarn ts-node <path-to-this-file> [options]
+
+  Options
+    --help,         Displays this message
+    --endpoint, -e  Specify endpoint of the form "address:port", defaults to localhost:20000
+`,
+    {
+        importMeta: import.meta,
+        flags: {
+            endpoint: {
+                type: 'string',
+                alias: 'e',
+                default: 'localhost:20000',
+            },
+        },
+    }
+);
+
+const [address, port] = cli.flags.endpoint.split(':');
+const client = createConcordiumClient(
+    address,
+    Number(port),
+    credentials.createInsecure()
+);
+
+/**
+ * Shuts down the node.
+ */
+
+(async () => {
+    await client.shutdown();
+
+    console.log('Node succesfully shut down');
+})();

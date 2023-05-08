f5da3889bd80ab0cc62545074b09889aca401a0e
Rasmus Kirk <mail@rasmuskirk.com>
2023-05-08 08:18:29 +0000

    Update packages/common/src/types/errors.ts
    Co-authored-by: Søren Bruus Zeppelin <soerenbf@gmail.com>

--- packages/common/src/types/errors.ts
+++ packages/common/src/types/errors.ts
@@ -3,11 +3,5 @@ import { RpcError } from '@protobuf-ts/runtime-rpc';
 export { RpcError };
 
 export function isRpcError(error: unknown): error is RpcError {
-    const err = error as RpcError;
-    return (
-        err.code !== undefined &&
-        err.meta !== undefined &&
-        err.name !== undefined &&
-        err.toString !== undefined
-    );
+    return error instanceof RpcError;
 }

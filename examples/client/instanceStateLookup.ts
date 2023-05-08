d6b852b1aabdca602852290017bf4433c46f64af
rasmus-kirk <mail@rasmuskirk.com>
2023-05-08 13:46:54 +0200

    Added more examples:
     - getTokenomicsInfo
     - instanceStateLookup
     - invokeContract
     - peerConnect
     - peerDisconnect
     - shutdown
     - unbanPeer

---
+++ examples/client/instanceStateLookup.ts
@@ -0,0 +1,78 @@
+import {
+    ContractAddress,
+    HexString,
+    createConcordiumClient,
+} from '@concordium/node-sdk';
+import { credentials } from '@grpc/grpc-js';
+
+import meow from 'meow';
+
+const cli = meow(
+    `
+  Usage
+    $ yarn ts-node <path-to-this-file> [options]
+
+  Required:
+    --key,      -k  The key from which to get the value from the contract state
+    --contract, -c  The index of the contract to query info about
+
+  Options
+    --help,         Displays this message
+    --block,    -b  A block to query from, defaults to last final block
+    --endpoint, -e  Specify endpoint of the form "address:port", defaults to localhost:20000
+`,
+    {
+        importMeta: import.meta,
+        flags: {
+            key: {
+                type: 'string',
+                alias: 'k',
+                isRequired: true,
+            },
+            contract: {
+                type: 'number',
+                alias: 'c',
+                isRequired: true,
+            },
+            block: {
+                type: 'string',
+                alias: 'b',
+                default: '', // This defaults to LastFinal
+            },
+            endpoint: {
+                type: 'string',
+                alias: 'e',
+                default: 'localhost:20000',
+            },
+        },
+    }
+);
+
+const [address, port] = cli.flags.endpoint.split(':');
+const client = createConcordiumClient(
+    address,
+    Number(port),
+    credentials.createInsecure()
+);
+
+/**
+ * Get the value at a specific key of a contract state as a hex string.
+ * In contrast to `GetInstanceState` this is more efficient, but requires the
+ * user to know the specific key to look for. If a blockhash is not supplied
+ * it will pick the latest finalized block.
+ */
+
+(async () => {
+    const contract: ContractAddress = {
+        index: BigInt(cli.flags.contract),
+        subindex: 0n,
+    };
+
+    const state: HexString = await client.instanceStateLookup(
+        contract,
+        cli.flags.key,
+        cli.flags.block
+    );
+
+    console.log(state);
+})();
---
+++ examples/client/peerDisconnect.ts
@@ -0,0 +1,58 @@
+import { createConcordiumClient } from '@concordium/node-sdk';
+import { credentials } from '@grpc/grpc-js';
+
+import meow from 'meow';
+
+const cli = meow(
+    `
+  Usage
+    $ yarn ts-node <path-to-this-file> [options]
+
+  Required
+    --ip,   -i  The ip of the peer to connect to.
+    --port, -p  The port of the peer to connect to.
+
+  Options
+    --help,         Displays this message
+    --endpoint, -e  Specify endpoint of the form "address:port", defaults to localhost:20000
+`,
+    {
+        importMeta: import.meta,
+        flags: {
+            ip: {
+                type: 'string',
+                alias: 'i',
+                isRequired: true,
+            },
+            port: {
+                type: 'number',
+                alias: 'p',
+                isRequired: true,
+            },
+            endpoint: {
+                type: 'string',
+                alias: 'e',
+                default: 'localhost:20000',
+            },
+        },
+    }
+);
+
+const [address, port] = cli.flags.endpoint.split(':');
+const client = createConcordiumClient(
+    address,
+    Number(port),
+    credentials.createInsecure()
+);
+
+/**
+ * Disconnect from the peer and remove them from the given addresses list
+ * if they are on it. Resolves if the request was processed successfully.
+ * Otherwise rejects.
+ */
+
+(async () => {
+    await client.peerDisconnect(cli.flags.ip, cli.flags.port);
+
+    console.log('Successfully disconnected from peer');
+})();

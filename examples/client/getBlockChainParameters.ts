0e54d41a4e5c00c25add35042bcd647299c73e5d
rasmus-kirk <mail@rasmuskirk.com>
2023-05-16 15:45:06 +0200

    Fixed splitting of endpoint string, now splits on last colon

--- examples/client/getBlockChainParameters.ts
+++ examples/client/getBlockChainParameters.ts
@@ -33,7 +33,11 @@ const cli = meow(
     }
 );
 
-const [address, port] = cli.flags.endpoint.split(':');
+// Split endpoint on last colon
+const lastColonIndex = cli.flags.endpoint.lastIndexOf(':');
+const address = cli.flags.endpoint.substring(0, lastColonIndex);
+const port = cli.flags.endpoint.substring(lastColonIndex + 1);
+
 const client = createConcordiumClient(
     address,
     Number(port),

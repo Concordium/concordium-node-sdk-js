377072cd7ff41b71b3635cc58a45d595d0d0d073
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-07-28 15:12:15 +0200

    Add test file for CIS4Contract

---
+++ packages/nodejs/test/CIS4Contract.test.ts
@@ -0,0 +1,9 @@
+import { ContractAddress } from '@concordium/common-sdk';
+
+const WEB3ID_ADDRESS: ContractAddress = {
+    index: 5463n,
+    subindex: 0n,
+};
+
+const TEST_BLOCK =
+    '8f3ce496605f497941d7834327ec4ea71800d93d271afaa58cb4ad7e460a38d7';

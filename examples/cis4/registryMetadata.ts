24d7ea11789381e99cadccd74cf27211ff503752
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-08-02 08:58:21 +0200

    Examples for cis4 view functions

---
+++ examples/cis4/registryMetadata.ts
@@ -0,0 +1,56 @@
+import meow from 'meow';
+import { credentials } from '@grpc/grpc-js';
+
+import { CIS4Contract, createConcordiumClient } from '@concordium/node-sdk';
+import { parseEndpoint } from '../shared/util';
+
+const cli = meow(
+    `
+  Usage
+    $ yarn run-example <path-to-this-file> [options]
+
+  Required
+    --index,            -i  The index of the smart contract
+
+  Options
+    --help,             -h  Displays this message
+    --endpoint,         -e  Specify endpoint of a grpc2 interface of a Concordium node in the format "address:port". Defaults to 'localhost:20000'
+    --subindex,             The subindex of the smart contract. Defaults to 0
+`,
+    {
+        importMeta: import.meta,
+        flags: {
+            endpoint: {
+                type: 'string',
+                alias: 'e',
+                default: 'localhost:20000',
+            },
+            index: {
+                type: 'number',
+                alias: 'i',
+                isRequired: true,
+            },
+            subindex: {
+                type: 'number',
+                default: 0,
+            },
+        },
+    }
+);
+
+const [address, port] = parseEndpoint(cli.flags.endpoint);
+const client = createConcordiumClient(
+    address,
+    Number(port),
+    credentials.createInsecure()
+);
+
+(async () => {
+    const contract = await CIS4Contract.create(client, {
+        index: BigInt(cli.flags.index),
+        subindex: BigInt(cli.flags.subindex),
+    });
+
+    const metadata = await contract.registryMetadata();
+    console.log('Metadata:', metadata);
+})();

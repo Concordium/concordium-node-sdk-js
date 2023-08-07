5c9fc31ff0819493fcb48f4030e770fd03671990
rasmus-kirk <mail@rasmuskirk.com>
2023-08-07 14:26:09 +0200

    Added helpers that convert between CCD and microCCD

--- packages/common/src/types/ccdAmount.ts
+++ packages/common/src/types/ccdAmount.ts
@@ -1,3 +1,5 @@
+import { Big } from 'big.js/';
+
 /**
  * Representation of a CCD amount.
  * The base unit of CCD is micro CCD, which is the representation
@@ -20,6 +22,44 @@ export class CcdAmount {
         }
 
         this.microCcdAmount = microCcdAmount;
+    }
+
+    /**
+     * Returns the amount of CCD as a string.
+     *
+     * @param useCommaDecimalSeperator Whether or not to use comma as a decimal seperator, defaults to false
+     * @returns The amount of CCD as a string
+     */
+    toCcd(useCommaDecimalSeperator = false): string {
+        const ccd = Big(this.microCcdAmount.toString()).div(Big(1000000));
+
+        if (useCommaDecimalSeperator) {
+            return ccd.toString().replace('.', ',');
+        } else {
+            return ccd.toString();
+        }
+    }
+
+    /**
+     * Parses an amount of CCD formatted as a string and creates a CcdAmount from it.
+     *
+     * @param ccd The amount of CCD to formatted as a string
+     * @param useCommaDecimalSeperator Whether or not to use comma as a decimal seperator, defaults to false
+     * @returns The CcdAmount object derived from the ccd input parameter
+     */
+    static fromCcd(ccd: string, useCommaDecimalSeperator = false): CcdAmount {
+        if (useCommaDecimalSeperator) {
+            const numberOfDecimalSeperators = ccd.split(',').length - 1;
+
+            if (numberOfDecimalSeperators > 1) {
+                throw Error('More than one decimal seperator found!');
+            }
+
+            ccd = ccd.replace('.', ',');
+        }
+
+        const microCcd = Big(ccd).mul(Big(1000000));
+        return new CcdAmount(BigInt(microCcd.toFixed()));
     }
 
     toJSON(): string {
---
+++ packages/common/test/types/ccdAmount.test.ts
@@ -0,0 +1,37 @@
+import { CcdAmount } from '../../src';
+
+describe('To and from ccd as strings', () => {
+    test('Parses one CCD correctly', () => {
+        const ccd = CcdAmount.fromCcd('1');
+        expect(ccd.microCcdAmount).toEqual(1000000n);
+    });
+
+    test('Parses decimals correctly', () => {
+        const ccd = CcdAmount.fromCcd('1.002');
+        expect(ccd.microCcdAmount).toEqual(1002000n);
+    });
+
+    test('CCD amounts that specifies more decimals than 6 throws', () => {
+        expect(() => CcdAmount.fromCcd('0.1234567')).toThrow();
+    });
+
+    test('CCD amounts that specifies 6 or less decimals is valid, test 1', () => {
+        const ccd = CcdAmount.fromCcd('0.123456');
+        expect(ccd.microCcdAmount).toEqual(123456n);
+    });
+
+    test('CCD amounts that specifies 6 or less decimals is valid, test 2', () => {
+        const ccd = CcdAmount.fromCcd('789.123456');
+        expect(ccd.microCcdAmount).toEqual(789123456n);
+    });
+
+    test('Returns correct amount of CCD, test 1', () => {
+        const ccd = new CcdAmount(1000n);
+        expect(ccd.toCcd()).toEqual('0.001');
+    });
+
+    test('Returns correct amount of CCD, test 2', () => {
+        const ccd = new CcdAmount(123456789n);
+        expect(ccd.toCcd()).toEqual('123.456789');
+    });
+});

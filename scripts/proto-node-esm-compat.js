336c0425258bae0d836c08b954e8b19abc06a02f
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-09-20 09:26:33 +0200

    All in on ESM

--- scripts/proto-node-esm-compat.js
+++ scripts/proto-node-esm-compat.js
@@ -2,7 +2,12 @@
 import { glob } from 'glob';
 import fs from 'fs';
 
-const protoRoot = 'src/grpc-api';
+const protoRoot = process.argv[2]; // First 2 arguments are node and path to this script. 3rd is expected to be location of generated proto type files.
+
+if (!protoRoot) {
+    throw new Error('Please supply a dir for the proto types to convert');
+}
+
 const files = await glob(protoRoot + '/**/*.ts');
 files.forEach((file) => {
     let content = fs.readFileSync(file, 'utf-8');

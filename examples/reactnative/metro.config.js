b3a6034f9741a865f2d0759000c833a81252466c
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-10-24 19:17:09 +0200

    react native example (ios works)

---
+++ examples/reactnative/metro.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  resolver: {
+    unstable_enableSymlinks: true, // This is only needed due to @concordium/web-sdk being symlinked.
+  },
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'),
+    path.resolve(__dirname, '../../node_modules/@concordium/web-sdk'),
+  ],
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);

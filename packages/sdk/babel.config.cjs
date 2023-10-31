9c5f7ddbea517b4803813b30d2e3e5f3b9c35195
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-10-31 09:24:45 +0100

    Explain babel file

--- packages/sdk/babel.config.cjs
+++ packages/sdk/babel.config.cjs
@@ -1,3 +1,7 @@
+/**
+ * Used for react-native tests configured in `jest.config.react-native.ts`
+ */
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],

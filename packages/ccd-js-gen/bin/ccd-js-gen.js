48acd46858d4ba4a15025fef85665eadfe3a17a1
Kasper Dissing Bargsteen <kb@concordium.com>
2023-11-30 16:31:28 +0100

    Fix executable on Windows
    `ps1` and `cmd` files are automatically generated from this script on Windows, but it interprets `-S` as the main program to run instead of `node`, which means that the scripts don't work.
    To get around it, I've changed the script so that it calls node without
    `--no-warnings` (and thus `-S`), but then it spawns a child process of
    `node --no-warnings` which executes the actual cli file.

--- packages/ccd-js-gen/bin/ccd-js-gen.js
+++ packages/ccd-js-gen/bin/ccd-js-gen.js
@@ -1,3 +1,36 @@
-#!/usr/bin/env -S node --no-warnings
-import { main } from '../lib/src/cli.js';
-main();
+#!/usr/bin/env node
+
+/**
+ * This file executes the `main` function in the `cli` file of the project.
+ *
+ * The execution immediately spawns a child process with node to get around some platform-specific issues.
+ * See https://github.com/Concordium/concordium-node-sdk-js/pull/315 for more details.
+ * */
+
+import { spawn } from 'node:child_process';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+import { pathToFileURL } from 'node:url';
+
+// Find the path to this script and resolve any symlinks.
+// Then use it to locate and execute the `cli.js` file while forwarding all arguments.
+const scriptPath = fileURLToPath(import.meta.url);
+const binFolder = path.parse(scriptPath).dir;
+const cliPath = pathToFileURL(path.join(binFolder, "..", "lib", "src", "cli.js"));
+const node = spawn('node', ['--no-warnings', '-e', `import("${cliPath}").then(cli => cli.main())`, "--", ...process.argv]);
+
+// Forward stdout from child process.
+node.stdout.on('data', (data) => {
+    console.log(`${data}`);
+});
+
+// Forward stderr from child process.
+node.stderr.on('data', (data) => {
+    console.error(`${data}`);
+});
+
+// Forward exit codes from child process.
+node.on('close', (code) => {
+    process.exit(code);
+});
+>>>>>>> Stashed changes

979b33f69e934e81efa1518202c2c68a01db1d4d
Lasse Møldrup <lasse.moeldrup@gmail.com>
2024-06-25 13:50:15 +0200

    Fix pipeline failure

--- packages/sdk/.size-limit.ts
+++ packages/sdk/.size-limit.ts
@@ -2,9 +2,9 @@ import type { SizeLimitConfig } from 'size-limit';
 
 module.exports = [
     {
-        name: "GRPC web client tree shaking",
-        path: "lib/esm/index.js",
-        limit: "54 KB",
-        import: "{ ConcordiumGRPCWebClient }"
-    }
+        name: 'GRPC web client tree shaking',
+        path: 'lib/esm/index.js',
+        limit: '54 KB',
+        import: '{ ConcordiumGRPCWebClient }',
+    },
 ] satisfies SizeLimitConfig;

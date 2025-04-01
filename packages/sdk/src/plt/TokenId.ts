1be875d8e971bb9269c58dac7612a826da9f99ec
Søren Bruus Zeppelin <soerenbf@gmail.com>
2025-04-01 14:54:58 +0200

    Check `TokenId` invariants

--- packages/sdk/src/plt/TokenId.ts
+++ packages/sdk/src/plt/TokenId.ts
@@ -3,15 +3,61 @@
  */
 export type JSON = string;
 
+const MAX_LENGTH = 256;
+
+/**
+ * Enum representing the types of errors that can occur with token amounts.
+ */
+export enum ErrorType {
+    /** Error type indicating the length of module reference is incorrect. */
+    EXCEEDS_MAX_LENGTH = 'EXCEEDS_MAX_LENGTH',
+}
+
+/**
+ * Custom error to represent issues with token amounts.
+ */
+export class Err extends Error {
+    private constructor(
+        /** The {@linkcode ErrorType} of the error. Can be used as to distinguish different types of errors. */
+        public readonly type: ErrorType,
+        message: string
+    ) {
+        super(message);
+        this.name = `TokenId.Err.${type}`;
+    }
+
+    /**
+     * Creates a TokenId.Err indicating the length of module reference is incorrect.
+     */
+    public static incorrectLength(): Err {
+        return new Err(
+            ErrorType.EXCEEDS_MAX_LENGTH,
+            `Token ID's cannot be longer than ${MAX_LENGTH} utf-8 encoded bytes`
+        );
+    }
+}
+
 /**
  * Protocol level token (PLT) ID.
  */
 class TokenId {
     /** Having a private field prevents similar structured objects to be considered the same type (similar to nominal typing). */
     private readonly __type = 'PLT.Id';
-    constructor(public readonly value: string) {
-        // TODO: invariants check
-        // - 256 utf8 bytes max?
+
+    /**
+     * Constructs a new TokenId instance.
+     * Validates that the value is smaller than the allowed utf-8 byte length.
+     *
+     * @throws {Err} If the value is longer than 256 utf-8 encoded bytes.
+     */
+    constructor(
+        /** The inner value */
+        public readonly value: string
+    ) {
+        // Check if the value exceeds 256 UTF-8 bytes
+        if (new TextEncoder().encode(value).length > MAX_LENGTH) {
+            throw Err.incorrectLength();
+        }
     }
 
     /**
@@ -38,8 +84,10 @@ export type Type = TokenId;
 
 /**
  * Create a protocol level token ID from a string value.
+ *
  * @param {string} value - The string to create the token ID from.
  * @returns {TokenId} A new token ID instance.
+ * @throws {Err} If the value is longer than 256 utf-8 encoded bytes.
  */
 export function fromString(value: string): TokenId {
     return new TokenId(value);
@@ -57,8 +105,10 @@ export function instanceOf(value: unknown): value is TokenId {
 
 /**
  * Converts {@linkcode JSON} to a token amount.
+ *
  * @param {string} json The JSON representation of the CCD amount.
  * @returns {CcdAmount} The CCD amount.
+ * @throws {Err} If the value is longer than 256 utf-8 encoded bytes.
  */
 export function fromJSON(json: JSON): TokenId {
     return fromString(json);

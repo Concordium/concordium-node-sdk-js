00a853510e3e42f9d09aba25c04e813696d35d7f
Thomas Dinsdale-Young <ty@concordium.com>
2025-08-29 14:18:22 +0200

    Try to fix CI again.

--- packages/sdk/scripts/run-protoc.js
+++ packages/sdk/scripts/run-protoc.js
@@ -1,5 +1,6 @@
 import { spawnSync } from "child_process";
 import path from "path";
+import fs from "fs";
 
 const isWindows = process.platform === "win32";
 
@@ -11,13 +12,17 @@ const protocGenTs = isWindows ?
     path.resolve("../../node_modules/@protobuf-ts/plugin/bin", "protoc-gen-ts");
 
 const grpcApiDir = path.resolve("../../deps/concordium-base/concordium-grpc-api");
+const protoDir = path.join(grpcApiDir, "v2", "concordium");
+const protoFiles = fs.readdirSync(protoDir)
+    .filter((file) => file.endsWith(".proto"))
+    .map((file) => path.join(protoDir, file));
 
 const args = [
     `--plugin=protoc-gen-ts=${protocGenTs}`,
     "--ts_opt", "optimize_code_size",
     "--ts_out=src/grpc-api",
     "-I", grpcApiDir,
-    `${grpcApiDir}/v2/concordium/*.proto`,
+    ...protoFiles,
 ];
 
 const result = spawnSync(protoc, args, { stdio: 'inherit', shell: isWindows });

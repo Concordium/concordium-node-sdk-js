f0b14b257ba0bb3978ff2f022499f79b8eb2d7e8
Richard B <richardb@Richards-MacBook-Pro.local>
2025-09-19 11:17:06 +0100

    added type module instead in package.json

--- packages/rust-bindings/ts-src/dapp.ts
+++ packages/rust-bindings/ts-src/dapp.ts
@@ -1,17 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { Buffer } from 'buffer/index.js';
 
+import { initSync } from '../lib/dapp/web/esm';
 import wasmBase64 from '../lib/dapp/web/esm/index_bg.wasm';
 
 // Expected to resolve to base64 encoded bytes of wasm module
 
-async function init() {
-    const { initSync } = await import('../lib/dapp/web/esm/index.js');
-    const bytes = Buffer.from(wasmBase64 as unknown as string, 'base64');
-    initSync(bytes);
+const bytes = Buffer.from(wasmBase64 as unknown as string, 'base64');
+initSync(bytes);
 
-    // If you need to export things from that module:
-    return import('../lib/dapp/web/esm/index.js');
-}
-
-export const walletModule = init();
+export * from '../lib/dapp/web/esm';
--- packages/rust-bindings/ts-src/wallet.ts
+++ packages/rust-bindings/ts-src/wallet.ts
@@ -1,17 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { Buffer } from 'buffer/index.js';
 
+import { initSync } from '../lib/wallet/web/esm';
 import wasmBase64 from '../lib/wallet/web/esm/index_bg.wasm';
 
 // Expected to resolve to base64 encoded bytes of wasm module
 
-async function init() {
-    const { initSync } = await import('../lib/wallet/web/esm/index.js');
-    const bytes = Buffer.from(wasmBase64 as unknown as string, 'base64');
-    initSync(bytes);
+const bytes = Buffer.from(wasmBase64 as unknown as string, 'base64');
+initSync(bytes);
 
-    // If you need to export things from that module:
-    return import('../lib/wallet/web/esm/index.js');
-}
-
-export const walletModule = init();
+export * from '../lib/wallet/web/esm';

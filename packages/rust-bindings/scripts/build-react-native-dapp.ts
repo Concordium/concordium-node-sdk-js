ec81176fd9c13aad50568a6a31dff5c0d852eb09
Richard B <richardb@Richards-MacBook-Pro.local>
2025-09-19 12:14:04 +0100

    fixing __dirname issue

--- packages/rust-bindings/scripts/build-react-native-dapp.ts
+++ packages/rust-bindings/scripts/build-react-native-dapp.ts
@@ -1,4 +1,8 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import { convertWasmToJs, copyToFolder } from './build-scripts.js';
 

31340102f43d0cc3bc92e7ff77b793f0cd84ecdb
Søren Bruus Zeppelin <soerenbf@gmail.com>
2025-10-01 12:24:30 +0200

    Use tsx to build rust-bindings

--- packages/rust-bindings/ts-src/dapp.ts
+++ packages/rust-bindings/ts-src/dapp.ts
@@ -5,8 +5,7 @@ import { initSync } from '../lib/dapp/web/esm/index.js';
 import wasmBase64 from '../lib/dapp/web/esm/index_bg.wasm';
 
 // Expected to resolve to base64 encoded bytes of wasm module
-
-const bytes = Buffer.from(wasmBase64 as unknown as string, 'base64');
+const bytes = Buffer.from(wasmBase64, 'base64');
 initSync(bytes);
 
 export * from '../lib/dapp/web/esm/index.js';
--- packages/rust-bindings/ts-src/wallet.ts
+++ packages/rust-bindings/ts-src/wallet.ts
@@ -5,8 +5,7 @@ import { initSync } from '../lib/wallet/web/esm/index.js';
 import wasmBase64 from '../lib/wallet/web/esm/index_bg.wasm';
 
 // Expected to resolve to base64 encoded bytes of wasm module
-
-const bytes = Buffer.from(wasmBase64 as unknown as string, 'base64');
+const bytes = Buffer.from(wasmBase64, 'base64');
 initSync(bytes);
 
 export * from '../lib/wallet/web/esm/index.js';

033de8dab97e7b34d35030317594196d711ec898
Harshitha <harshitha@creativeideaz.io>
2026-02-11 18:24:54 +0530

    feat: releasing ui and functional updates for verification web ui

---
+++ packages/verification-web-ui/postcss.config.ts
@@ -0,0 +1,11 @@
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+import postcssRemoveEmptyLayers from './postcss-remove-empty-layers.js';
+
+export default {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    postcssRemoveEmptyLayers,
+  ],
+};

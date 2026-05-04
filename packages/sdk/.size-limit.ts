a1262445917f037a06142650a53b8f1ec6c8c1eb
Søren Bruus Zeppelin <soerenbf@gmail.com>
2026-05-04 10:56:58 +0200

    CI fix

--- packages/sdk/.size-limit.ts
+++ packages/sdk/.size-limit.ts
@@ -4,7 +4,7 @@ module.exports = [
     {
         name: 'GRPC web client tree shaking',
         path: 'lib/esm/index.js',
-        limit: '62 KB',
+        limit: '63 KB',
         import: '{ ConcordiumGRPCWebClient }',
     },
 ] satisfies SizeLimitConfig;
